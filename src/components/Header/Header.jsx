f0e4d22aea07794d589ba32b52a52c3903001e08
fallenreds <artur.khobotkov@nure.ua>
2023-01-23 08:01:34 +0300

    Посты начало

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,10 +10,7 @@ const Header = () => {
 
     return (
         <div className={'header'}>
-            <Button onClick={onClose}>Закрыть</Button>
-            <span className={'username'}>
-                {user?.username}
-            </span>
+
         </div>
     );
 };
