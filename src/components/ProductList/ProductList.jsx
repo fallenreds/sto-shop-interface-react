dcd497748ee396cd595d43633863fd2925dde665
fallenreds <artur.khobotkov@nure.ua>
2023-01-23 08:11:25 +0300

    Посты начало

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+/*import React, {useState} from 'react';*/
 import './ProductList.css';
 import Button from "../Button/Button";
 
@@ -7,7 +7,7 @@ import Button from "../Button/Button";
 
 const ProductList = () => {
 
-    const [search, setSearch] = useState('');
+   /* const [search, setSearch] = useState('');
     const [category, setCategory] = useState('');
     const onSearch = (e) => {
         setSearch(e.target.value)
@@ -15,7 +15,7 @@ const ProductList = () => {
 
     const onChangeCategory = (e) => {
         setCategory(e.target.value)
-    }
+    }*/
     return (
         <div className={"form"}>
             <h3>Выбирайте любые подходящие вам товары. Покупайте не выходя из Telegram!</h3>
@@ -23,24 +23,24 @@ const ProductList = () => {
                 className={'input'}
                 type="text"
                 placeholder={'Поиск'}
-                value={search}
-                onChange={onSearch}
+                value=/*{search}*/'value'
+               /* onChange={onSearch}*/
                 />
-            <select value={category} onChange={onChangeCategory} className={'select'}>
-                <option value={'1'}>Категория 1</option>
-                <option value={'2'}>Категория 2</option>
-                <option value={'3'}>Категория 1.1</option>
-                <option value={'3'}>Категория 1.2</option>
+            <select value='category'/*{category}*/ /*onChange={onChangeCategory}*/ className={'select'}>
+                <option value='1'>Категория 1</option>
+                <option value='2'>Категория 2</option>
+                <option value='3'>Категория 1.1</option>
+                <option value='3'>Категория 1.2</option>
             </select>
 
             <div className={"postlist"}>
                 <div className={'post'}>
-                    <img src="https://storage.remonline.app/warehouse_images/fb41c9db9d3f48e39875e63b15dfe530_500_500.jpeg?"/>
+                    <img alt='Фото' src="https://storage.remonline.app/warehouse_images/fb41c9db9d3f48e39875e63b15dfe530_500_500.jpeg?"/>
                     Название какого-то товара<p/>
                     <Button>В корзину</Button>
                 </div>
                 <div className={'post'}>
-                    <img src="https://storage.remonline.app/warehouse_images/9f1204e21c4f4beeaaeb5a25faaf26b0_500_500.jpeg?"/>
+                    <img alt='Фото' src="https://storage.remonline.app/warehouse_images/9f1204e21c4f4beeaaeb5a25faaf26b0_500_500.jpeg?"/>
                     Название какого-то товара
                     <Button>В корзину</Button>
                 </div>
