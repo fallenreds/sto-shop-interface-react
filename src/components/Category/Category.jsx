a1b54b403810ff6de34f5bd8d0b79a140fffbe03
fallenreds <artur.khobotkov@nure.ua>
2023-01-24 21:46:33 +0300

    Сетка категорий траблы с картинками

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -3,7 +3,7 @@ import classes from "./Category.module.css";
 const Category = (props) => {
     return (
         <div className={classes.category}>
-            <img alt ='' src="https://i.etsystatic.com/20556998/r/il/45e102/2835384837/il_570xN.2835384837_cwi9.jpg"/>
+            <img src="../../images/category-logo/acura.jpg" alt ='img' />
             Категория
         </div>
     );
