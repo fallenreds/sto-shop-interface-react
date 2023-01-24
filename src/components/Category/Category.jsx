e90c13a5b69c9ef2c28c6e4a9571a8587272cc87
fallenreds <artur.khobotkov@nure.ua>
2023-01-24 08:40:48 +0300

    Сетка категорий

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -3,7 +3,7 @@ import classes from "./Category.module.css";
 const Category = (props) => {
     return (
         <div className={classes.category}>
-            <img src="https://i.etsystatic.com/20556998/r/il/45e102/2835384837/il_570xN.2835384837_cwi9.jpg"/>
+            <img alt ='' src="https://i.etsystatic.com/20556998/r/il/45e102/2835384837/il_570xN.2835384837_cwi9.jpg"/>
             Категория
         </div>
     );
