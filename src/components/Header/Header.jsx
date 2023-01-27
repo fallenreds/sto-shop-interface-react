d4101c64f773ec122bd194b86528b084089b12b8
fallenreds <artur.khobotkov@nure.ua>
2023-01-27 12:21:07 +0300

    Все или ничего

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,18 +4,16 @@ import {useTelegram} from "../../hooks/useTelegram";*/
 import classes from "./Header.module.css"
 // import ProductList from "../ProductList/ProductList";
 // import shopingCartList from "../ShopingCartList/ShopingCartList";
-import {Link} from "react-router-dom";
-
-
+import {useHistory} from 'react-router-dom'
 
 const Header = () => {
-
-    function goShoppingCart() {
-        window.Telegram.WebApp.openLink('/schart')
-    }
-    function goProductlist() {
-        window.Telegram.WebApp.openLink('/')
-    }
+    const router = useHistory()
+    // function goShoppingCart() {
+    //     window.Telegram.WebApp.openLink('')
+    // }
+    // function goProductlist() {
+    //     window.Telegram.WebApp.openLink('/')
+    // }
     return (
         <div className={classes.header}>
             <input
@@ -26,10 +24,10 @@ const Header = () => {
                 /* onChange={onSearch}*/
             />
             <div className={classes.shopping_cart}>
-                <button onClick={goProductlist}><img src={require("../../product-list.png")} alt ='img'/></button>
+                <button onClick={()=>router.push('/')}><img src={require("../../product-list.png")} alt ='img'/></button>
             </div>
             <div className={classes.shopping_cart}>
-                <button onClick={goShoppingCart}><img src={require("../../shopping_cart_incon.png")} alt ='img'/></button>
+                <button onClick={()=>router.push('/shcart')}><img src={require("../../shopping_cart_incon.png")} alt ='img'/></button>
             </div>
 
         </div>
