1338a449db380636ef38d565cfaf0e3b01c4e0b6
fallenreds <artur.khobotkov@nure.ua>
2023-01-27 19:31:13 +0300

    restart-category

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,6 +1,6 @@
 /*import React, {useState} from 'react';*/
 import './ProductList.css';
-
+import category from "../../category.json";
 import data from '../../goods.json'
 import Good from "../Good/Good";
 import SimpleSlider from "../../UI/Slider";
@@ -9,21 +9,32 @@ import {useState} from "react";
 
 
 
-const ProductList = (props) => {
+const ProductList = () => {
 
     const [categoryState, setCategory] = useState(null)
     function categoryFilter(categoryId) {
         setCategory(categoryId)
     }
+    function restartCategory() {
+        categoryFilter(null)
+    }
+
+    let categoryLabel = categoryState === null ? "Все товары":
+                        category.data.find(item=>item.id === categoryState).title
+
+
 
 
     let filterGoodByCategory = categoryState === null ? data.data: data.data.filter(item=>item.category.id===categoryState)
 
-    console.log(filterGoodByCategory)
     return (
         <div className={"form"}>
             <SimpleSlider category={categoryFilter}/>
-            <div className={"maintext"}><h3>Все товары</h3></div>
+            <div className={"maintext"}>
+                <h3>{categoryLabel}<button  onClick={restartCategory} style={{background:"none", border: "none"}}>❎</button></h3>
+
+            </div>
+
             <div className={"postlist"}>
 
                 {filterGoodByCategory.map(good=> <Good
